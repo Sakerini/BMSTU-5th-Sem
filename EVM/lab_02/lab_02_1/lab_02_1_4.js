0672798c3bb7b7ce888c17b4622f658086cb3558
Sakerini <sakerini1999@gmail.com>
2020-10-05 14:29:01 +0300

    Lab EVM sdana

--- EVM/lab_02/lab_02_1/lab_02_1_4.js
+++ EVM/lab_02/lab_02_1/lab_02_1_4.js
@@ -7,11 +7,26 @@ const folderPath = readlineSync.question("input path to folder for scan:")
 
 const fileNames = fs.readdirSync(folderPath);
 
+function checkIfDir(fileName) {
+    try {
+        fs.readFileSync(fileName, "utf8");
+        return false
+    } catch (error) {
+        return true;
+    }
+}
+
 const extension = ".txt"
 function getAllFiles(folderPath, extension) {
     const fileNames = fs.readdirSync(folderPath);
     for(let i = 0; i < fileNames.length; i++) {
         const fileName = fileNames[i];
+        /*
+        if (checkIfDir(fileName)) {
+            getAllFiles(folderPath + fileName + "/", extension)
+        }
+        */
+        
         if (fileName.includes(extension)) {
             console.log(fileName);
         } else if (!fileName.includes(".")) {
@@ -21,4 +36,7 @@ function getAllFiles(folderPath, extension) {
 }
 
 getAllFiles(folderPath, extension)
+
+
+
 
--- EVM/lab_02/lab_02_2/lab_02_2_3.js
+++ EVM/lab_02/lab_02_2/lab_02_2_3.js
@@ -2,53 +2,60 @@
 
 const readlineSync = require('readline-sync');
 const fs = require("fs");
+const express = require("express");
 
+const app = express();
+const port = 5015;
+app.listen(port);
+console.log("My server on port " + port);
 
-const fields = [];
+app.get("/me/page", function(request, response) {
+    const nameString = request.query.p;
+    if (fs.existsSync(nameString)) {
+        const contentString = fs.readFileSync(nameString, "utf8");
+        response.end(contentString);
+    } else {
+        const contentString = fs.readFileSync("bad.html", "utf8");
+        response.end(contentString);
+    }
+});
 
-let value = true;
+app.get("/input", function(request, response) {
+    const address = request.query.address;
+    let fields = request.query.field
 
-while(value !== "") {
-    value = readlineSync.question("Input field name: ");
-    if(value !== "") fields.push(value);
-}
+    let htmlDoc = buildHtml(address, fields);
+    const fileName = "test.html";
+    fs.writeFileSync(fileName, htmlDoc);
+    console.log("Create File OK");
 
-const address = readlineSync.question("Input address: ");
+    const contentString = fs.readFileSync("test.html", "utf8");
+    response.end(contentString);
+});
 
-for(let i = 0; i < fields.length; i++) {
-    const message = i + ") " + fields[i];
-    console.log(message);
-}
-
-
-function buildRequestHtmlForm() {
-    var formTittle = "<form method=\"GET\" action=\""+ address> +"\">"
+function buildRequestHtmlForm(address, fields) {
+    var formTittle = "<form method=\"GET\" action=\""+ address +"\">"
     var formBody = "";
 
-    for (let i = 0; i < fields.length; i++) {
-        formBody += "<p>" + fields[i] + "</p>" +
-        "<input name =\"" + i + "\" spellcheck=\"false\" autocomplete=\"off\">"
+    if (!Array.isArray(fields)) {
+        formBody += "<p>" + fields + "</p>" +
+        "<input name =\"" + 1 + "\" spellcheck=\"false\" autocomplete=\"off\">"
+    } else {
+        for (let i = 0; i < fields.length; i++) {
+            formBody += "<p>" + fields[i] + "</p>" +
+            "<input name =\"" + i + "\" spellcheck=\"false\" autocomplete=\"off\">"
+        }
     }
     formBody += "<br><br><input type=\"submit\" value=\"Отправить запрос\"></form>"
-
+    console.log(formTittle)
     return formTittle+formBody
 }
 
-function buildHtml(req) {
+function buildHtml(address, fields) {
     var header = "<meta charset=\"UTF-8\"><title>Сгенерированый HTML</title>";
-    var form = buildRequestHtmlForm();
+    var form = buildRequestHtmlForm(address, fields);
+    console.log(form)
     var body = form;
-  
-    // concatenate header string
-    // concatenate body string
-  
     return '<!DOCTYPE html>'
          + '<html><head>' + header + '</head><body>' + body + '</body></html>';
   };
-
-let htmlDoc = buildHtml();
-
-const fileName = "test.html";
-fs.writeFileSync(fileName, htmlDoc);
-
-console.log("Create File OK");
